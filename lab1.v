23c0c9e62db9be393052592b0de1df86db0f0e73
hemapriya793-web <hemapriya793@gmail.com>
2025-11-29 15:41:45 +0530

    Add files via upload

---
+++ lab1.v
@@ -0,0 +1,13 @@
+// Half Adder in Verilog
+module lab1 (
+    input  wire a, b,     // Inputs
+    output wire sum,      // Sum output
+    output wire carry     // Carry output
+);
+
+    // Logic equations
+    assign sum   = a ^ b;   // XOR for sum
+    assign carry = a & b;   // AND for carry
+
+endmodule
+
